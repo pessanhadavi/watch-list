12cdf64c2fde77278fa3e730f147d1e10c9bb816
David Pessanha <davidppessanha@gmail.com>
2021-05-27 18:48:23 -0300

    List - create feature added

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,9 +12,22 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
