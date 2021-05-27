d5ccee8af845aebb9b5df16849768e94d2307c33
David Pessanha <davidppessanha@gmail.com>
2021-05-27 18:07:13 -0300

    List - show feature added

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,16 @@
 class ListsController < ApplicationController
+    before_action :set_list, only:[:show]
   def index
     @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.find_by(list_id: params[:id])
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
