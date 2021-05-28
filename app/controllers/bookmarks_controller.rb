df8b45fc3e47542a79cd3fa055498ddbbe7f3c3d
David Pessanha <davidppessanha@gmail.com>
2021-05-28 11:42:22 -0300

    Bookmark - destroy action, List - view, f.association in show.html

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,13 @@
 class BookmarksController < ApplicationController
   before_action :set_list, only: [:new, :create]
+  before_action :set_bookmark, only: [:destroy]
 
   def new
     @bookmark = Bookmark.new
   end
 
   def create
-    @bookmark = Bookmark.new(comment: bookmark_params[:comment], movie: find_movie)
+    @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
@@ -16,11 +17,8 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    raise
-    @list = List.find(params[:id])
-    @bookmark = Bookmark.find_by(list_id: params[:id])
     @bookmark.destroy
-    redirect_to list_path(@list)
+    redirect_to list_path(@bookmark.list)
   end
 
   private
@@ -29,12 +27,12 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
   end
 
-  def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie)
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 
-  def find_movie
-    Movie.find(bookmark_params[:movie])
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
   
 end
